65eecc86f70b2ac6872561275c6aef7dd49eef4c
Timur Nizamov <abc@nizamovtimur.ru>
2025-07-09 16:59:27 +0500

    refactor attacks descriptions

--- src/llamator/attacks/pair.py
+++ src/llamator/attacks/pair.py
@@ -50,14 +50,8 @@ class TestPAIR(TestBase):
             "arxiv:2310.08419v4",
         ],
         "description": {
-            "en": (
-                "Challenges the LLM with a dataset of HarmBench prompts using multistage refinement "
-                "judge model scoring."
-            ),
-            "ru": (
-                "Испытывает LLM набором запросов, связанных с вредоносным поведением, "
-                "используя самоулучшение атакующей модели с оценкой модели-судьи."
-            ),
+            "en": "Challenges the LLM with a dataset of HarmBench prompts using multistage refinement judge model scoring. Original Paper: https://arxiv.org/abs/2310.08419v4, Code: https://github.com/patrickrchao/JailbreakingLLMs",
+            "ru": "Испытывает LLM набором запросов, связанных с вредоносным поведением, используя самоулучшение атакующей модели с оценкой модели-судьи. Статья: https://arxiv.org/abs/2310.08419v4, репозиторий: https://github.com/patrickrchao/JailbreakingLLMs",
         },
         "github_link": "https://github.com/LLAMATOR-Core/llamator/blob/release/src/llamator/attacks/pair.py",
     }
